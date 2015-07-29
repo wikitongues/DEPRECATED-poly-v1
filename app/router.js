87bb148ee87b2c69c221fff2e45069c6d98679dc
Freddie Andrade <faandrade@gmail.com>
2015-07-29 01:37:46 -0400

    route/resource blah

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('book', {path:'books/:book_id'});
+  this.resource('book', {path:'/books/:book_id'});
   this.route('new');
 });
 
