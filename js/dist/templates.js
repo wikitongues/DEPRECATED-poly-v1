d1b14ff025ddc4bedfb1767cbe8114dd0e21b49d
Freddie Andrade <faandrade@gmail.com>
2015-07-29 17:24:15 -0400

    tweaks

--- js/dist/templates.js
+++ js/dist/templates.js
@@ -322,9 +322,9 @@ function program1(depth0,data) {
   stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n<button>\n  <img class=\"icon more\" src=\"img/icons/i_more.svg\" alt=\"more\">\n</button>\n");
-  data.buffer.push(escapeExpression(helpers.view.call(depth0, "Ember.TextField", {hash:{
+  data.buffer.push(escapeExpression((helper = helpers.input || (depth0 && depth0.input),options={hash:{
     'placeholder': ("Search for a tag, phrase or language")
-  },hashTypes:{'placeholder': "STRING"},hashContexts:{'placeholder': depth0},contexts:[depth0],types:["ID"],data:data})));
+  },hashTypes:{'placeholder': "STRING"},hashContexts:{'placeholder': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "input", options))));
   return buffer;
   
 });
