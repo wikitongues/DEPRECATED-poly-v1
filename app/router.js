686d096d7e08f2a2b10b9730d40df67184af147a
Fredward <faandrade@gmail.com>
2015-08-10 23:15:33 -0400

    fixed!!!

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('book', {path:'/books/:book_id'});
+  this.route('book', {path:'/books/:book_id'});
   this.route('new');
 });
 
