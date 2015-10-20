a8f047a6476c1dc1bf2880bf055aa68a074d47c8
Freddie Andrade <faandrade@gmail.com>
2015-10-19 21:30:21 -0400

    refactor book controller pasta code, mobile styles

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -19,33 +19,31 @@ export default Ember.Controller.extend({
     },
 
     addPhrase: function() {
-      console.log("hi")
-      var progressElement = "<li class='entry'><ul><li class='source'><p><span class='progress'><span></span><span></span><span></span></span></p></li></ul></li>"
-      // Ember.$(".addPhrase").toggleClass("open");
+      var progressElement = "<li class='entry' id='progressElement'><ul><li class='source'><p><span class='progress'><span></span><span></span><span></span></span></p></li></ul></li>"
+      Ember.$(".addPhrase").toggleClass("open");
       Ember.$(".newPhrase").toggleClass("open");
       Ember.$(".book .content-wrapper ul.content").append(progressElement);
+      // create record
     },
 
     saveSource: function() {
+      var sourceVal = Ember.$(".newPhrase .input.source").val(),
+      progressElement = "<li class='target'><p><span class='progress'><span></span><span></span><span></span></span></p></li>"
       if(Ember.$(".newPhrase .input.source").val()!=="") {
-        Ember.$(".book .content-wrapper ul.content .entry:last-of-type ul li.source p").html(Ember.$(".newPhrase .input.source").val()).parent().parent().append("<li class='target'><p><span class='progress'><span></span><span></span><span></span></span></p></li>");
-        Ember.$(".newPhrase .input.source").hide();
-        Ember.$(".newPhrase .input.target").show();
-        Ember.$(".newPhrase .saveSource").hide();
-        Ember.$(".newPhrase .saveTarget").show();
+        Ember.$("#progressElement li.source p").html(sourceVal)
+        Ember.$("#progressElement ul").append(progressElement);
+        Ember.$(".newPhrase").children("input.source, .saveSource").hide()
+        Ember.$(".newPhrase").children("input.target, .saveTarget").show()
         Ember.$(".newPhrase .input.source").val("");
       }
     },
 
     saveTarget: function() {
       if(Ember.$(".newPhrase .input.target").val()!=="") {
-        Ember.$(".newPhrase .input.source").show();
-        Ember.$(".newPhrase .input.target").hide();
-        Ember.$(".newPhrase .saveSource").show();
-        Ember.$(".newPhrase .saveTarget").hide();
+        Ember.$(".newPhrase").children("input.target, .saveTarget").hide()
+        Ember.$(".newPhrase").children("input.source, .saveSource").show()
         Ember.$(".newPhrase .input.target").val("");
-        Ember.$(".newPhrase").toggleClass("open");
-        Ember.$(".addPhrase").toggleClass("open");
+        Ember.$(".newPhrase, .addPhrase").toggleClass("open");
 
         var phrase = this.store.createRecord('phrase',{
           book: this.get('model'),
@@ -61,8 +59,9 @@ export default Ember.Controller.extend({
           controller.get("model.phrases").addObject(phrase);
         });
         this.get("model").save();
+
       }
-      Ember.$(".book .content-wrapper ul.content .entry:last-of-type").remove();
+      Ember.$("#progressElement").remove();
     }
   }
 });
